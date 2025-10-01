d3ad8928b44bf696c981fd62edeb33bd346a4010
justHman <hmantools11@gmail.com>
2025-10-01 11:27:35 +0700

    Done modules

--- src/model.py
+++ src/model.py
@@ -56,17 +56,51 @@ class C2f(nn.Module):
 
         self.conv1 = Conv(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
         self.bottlenecks = nn.ModuleList(
-            [Bottleneck(self.mid_channels, self.mid_channels) for _ in range(n_bottlenecks)]
+            [Bottleneck(self.mid_channels, self.mid_channels, shortcut) for _ in range(n_bottlenecks)]
         )
         self.conv2 = Conv((n_bottlenecks + 2) * out_channels // 2, out_channels, kernel_size=1, stride=1, padding=0)
                     
     def forward(self, x):
         x = self.conv1(x)
-        x1, x2 = x.chunk(self.mid_channels, dim=1)
+        x1, x2 = x.chunk(2, dim=1)
 
         for bottleneck in self.bottlenecks:
             x1 = bottleneck(x1)
             x = torch.cat([x, x1], dim=1)
-        
         x = self.conv2(x)
         return x
+
+class SPPF(nn.Module):
+    def __init__(
+            self,
+            in_channels, out_channels,
+            kernel_size=5
+        ):
+        super().__init__()
+        self.mid_channels = in_channels // 2
+        self.conv1 = Conv(in_channels, self.mid_channels, kernel_size=1, stride=1, padding=0)
+        self.maxpool = nn.MaxPool2d(kernel_size=kernel_size, stride=1, padding=kernel_size // 2, ceil_mode=False, dilation=1)
+        self.conv2 = Conv(self.mid_channels * 4, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        y = x
+        for _ in range(3):
+            y = self.maxpool(y)
+            x = torch.cat([x, y], dim=1)
+        x = self.conv2(x)
+        return x
+    
+if __name__ == "__main__":
+    c2f = C2f(64, 128, n_bottlenecks=2)
+    print(f"{sum(p.numel() for p in c2f.parameters())/1e6} parameters")
+
+    x = torch.randn(1, 64, 32, 32)
+    y = c2f(x)
+    print(y.shape)
+
+    sppf = SPPF(128, 256, kernel_size=5)
+    print(f"{sum(p.numel() for p in sppf.parameters())/1e6} parameters")
+
+    y1 = sppf(y)
+    print(y1.shape)
